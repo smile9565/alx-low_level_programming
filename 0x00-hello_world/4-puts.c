0b9fed245f47689499ee4f703185f225ac6a08fb
smile9565 <bad9565@gmail.com>
2022-09-09 07:16:51 -0700

    bet

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
+ * Description: main - Prints out a string to stdout.
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * Return: 0 if success.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
