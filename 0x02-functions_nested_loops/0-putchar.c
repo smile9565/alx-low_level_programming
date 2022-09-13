ba1122ba0e2e9ef7417e1c6a06faebd7effe736a
smile9565 <bad9565@gmail.com>
2022-09-13 10:09:46 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "alx.h" 
+ /** 		
+  * main - Entry point 
+  * 
+  * Return: Always 0 (Success) 
+  */ 
+ int main(void) 
+ {				 
+         char *sh = "Alx"; 
+  
+         while (*sh) 
+         { 
+                 _putchar(*sh); 
+                 sh++; 
+         } 
+         _putchar('\n'); 
+  
+         return (0); 
+ }i0x02-functions_nested_loops
