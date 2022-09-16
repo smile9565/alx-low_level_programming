540744dd5cce190aedd5a96a3c864baa8e4a9ddc
smile9565 <bad9565@gmail.com>
2022-09-16 01:31:13 -0700

    task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,12 @@
   * 
   * Description: Prints the alphabet with _putchar 
   * @c: charater 
-  * Return: void 
+  * Return: void 1 if 'c' is uppercase otherwise always 0 (sucess)
   */ 
  				 
  int _isupper(int c) 
  { 
-         if (c >= 'A' && c <= 'Z') 
+         if (c >= 65 && c <= 90) 
                  return (1); 
          else 
                  return (0); 
