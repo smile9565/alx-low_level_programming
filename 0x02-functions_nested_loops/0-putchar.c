f34ad9c8292060c5e652eec9b564a8e76e3b8220
smile9565 <bad9565@gmail.com>
2022-09-16 02:11:18 -0700

    pro2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
-#include "main.h"
- /** 				
-  * main - Entry point 
-  * 
-  * Return: Always 0 (Success) 
-  */ 
- int main(void) 
- {							 
-         char a,
-	 while (_putchar ;)
-	 {			
-		 _putchar(a);		
-	 }
-             putchr(\n);		
-        			
-  
-         return (0); 				
- }
+#include "main.h"
+/**
+ * main - Prints main
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
