8318c8405fb680cbba32a4df602e94665b8a86ec
smile9565 <bad9565@gmail.com>
2022-09-16 01:20:26 -0700

    task0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H		
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include<stdio.h>
 
 int _putchar(char c); 
 int _isupper(int c); 
@@ -12,5 +14,6 @@ void print_line(int n);
 void print_diagonal(int n); 
 void print_square(int size); 
 void print_triangle(int size); 
+void print_number(int n);
 
 #endif
