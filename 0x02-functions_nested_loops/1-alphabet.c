98906f7d0201cfb40d817265ac321f5a836a45f7
smile9565 <bad9565@gmail.com>
2022-09-16 02:26:09 -0700

    project2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,19 @@
-#include "main.h"
- /** 
-  * print_alphabet - prints the english alphabet from a-z. 
-  * Return: Nothing. 
-  */ 
- void print_alphabet(void) 
- { 
-         char c; 
-  			
-         for (c = 'a'; c <= 'z'; c++) 
-         { 
-                 _putchar(c); 
-         } 
-         _putchar('\n'); 
- }
+#include "main.h"
+
+/**
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
+
