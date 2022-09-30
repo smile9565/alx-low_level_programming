f431b533fc426a63a5d562ce43bda91f84c87c40
smile9565 <bad9565@gmail.com>
2022-09-30 11:56:21 -0700

    argiv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include<stdio.h> 
+ 	 
+ /** 
+  * main - Prints name of command called 
+  * @argc: number of arguments including command name 
+  * @argv: Pointer to array of pointers to arguments as strings including 
+  *                        command name 
+  * 
+  * Return: 0 for success, 1 for error 
+  */ 
+ int main(int argc, char **argv) 
+ { 
+         if (!argc) 
+                 return (1); 
+  
+         printf("%s\n", *argv); 
+  
+         return (0); 
+ }
