e04e3a0bfd4f5a1c06f904ea2083bdca9a713eb5
smile9565 <bad9565@gmail.com>
2022-10-05 08:44:59 -0700

    static_libraries

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,24 @@
+/**
+ * *_strchr - program startup
+ * @s: pointer being evaluated
+ * @c: char being evaluated
+(*
+ * Description: locates a characer in a string)?
+ * Return: return (0) is the required function signature
+ */
+
+#include "main.h"
+#include <stddef.h>
+char *_strchr(char *s, char c)
+{
+	int character;
+
+	for (character = 0; s[character] >= '\0'; character++)
+	{
+		if (s[character] == c)
+		{
+			return (s + character);
+		}
+	}
+	return (NULL);
+}
