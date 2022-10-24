101a44acabfc482826e9cc384df2fea66b63b717
smile9565 <bad9565@gmail.com>
2022-10-24 04:26:34 -0700

    singly linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,24 +1,23 @@
-#include "lists.h" 
-  
- /** 
-  * print_list - prints all the elements of a list_t list 
-  * @h: pointer 
-  * Return: number of nodes in the list 
-  */ 
- size_t print_list(const list_t *h) 
- { 
-         size_t len = 0; 
-         int i; 
-         const list_t *c = h; 
-  
-         for (i = 0; c; i++) 
-         { 
-                 if (c->str == NULL) 
-                         printf("[0] (nil)\n"); 
-                 else 
-                         printf("[%d] %s\n", c->len, c->str); 
-         c = c->next; 
-         len++; 
-         } 
- return (len); 
- }
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * @h: a variable pointing to a struct of type list_t
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		printf("[%d] %s\n", h->len, h->str);
+		else
+		printf("[0] (nil)\n");
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,15 @@
-#include "lists.h" 
- /** 
-  * Before - marks printf to execute before main 
-  */ 
- void Before(void) __attribute__ ((constructor)); 
- void Before(void) 
- { 
-         printf("You're beat! and yet, you must allow,\n" 
-                "I bore my house upon my back!\n"); 
- }
+SECTION .data
+msg:	    db "Hello, Holberton", 0
+fmt:	    db "%s", 10, 0
+
+	        SECTION .text
+	        extern printf
+	        global main
+main:
+	        mov esi, msg
+	        mov edi, fmt
+	        mov eax, 0
+	        call printf
+
+	        mov eax, 0
+	ret
