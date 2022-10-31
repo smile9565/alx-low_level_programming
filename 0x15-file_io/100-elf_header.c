6124fd6c866b59f89441c88393ae266ebffb2132
Esmael Mohammed <bad9565@gmail.com>
2022-10-31 15:57:58 +0200

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,184 +1,46 @@
-#include "holberton.h"
-#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <fcntl.h>
+#include "main.h"
+#include <unistd.h>
 
-int main(int argc, char **argv)
+/**
+ * main - copy the content of a file to another file.
+ * @argc: number of arguments.
+ * @argv: arguments
+ * Return: 0 on sucess
+ */
+int main(int argc, char *argv[])
 {
-	ssize_t fd, read_res;
-	char buffer[16];
-	size_t i;
-	char match[4] = {0x7f, 'E', 'L', 'F'};
+
+	int fd;
+	ssize_t sz;
+	char buf[5];
+	char elf[1];
+
 
 	if (argc != 2)
-		err("Improper usage\n");
-
-	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-		err("Could not open file\n");
-
-	read_res = read(fd, buffer, 16);
-	if (read_res < 0)
-		err("Could not read from file\n");
-
-	for (i = 0; i < 4; i++)
 	{
-		if (buffer[i] != match[i])
-			err("Not an ELF file!\n");
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file\n");
+		exit(97); }
+	fd = open(argv[1], O_RDWR);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't open from file %s\n", argv[1]);
+		exit(98); }
+	sz = read(fd, buf, 4);
+	if (sz == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98); }
+	sz = lseek(fd, 1, SEEK_SET);
+	sz = read(fd, elf, 1);
+	if (elf[0] != 'E')
+		exit(98);
 
-	printf("ELF Header:\n");
-	printmagic(buffer);
-	printclass(buffer);
-	printdata(buffer);
-	printversion(buffer);
-	printos(buffer);
-	printabversion(buffer);
-
-	if (close(fd))
-		err("Could not close file");
-
+	close(fd);
 	return (0);
-}
-
-void err(char *msg)
-{
-	size_t len;
-
-	for (len = 0; msg[len]; len++)
-		;
-	write(STDERR_FILENO, msg, len);
-
-	exit(98);
-}
-
-void printabversion(char *buffer)
-{
-	ptitle("ABI Version");
-
-	printf("%d", buffer[8]);
-
-	printf("\n");
-}
-
-void printos(char *buffer)
-{
-	ptitle("OS/ABI");
-
-	switch (buffer[7])
-	{
-		case ELFOSABI_SYSV:
-			printf("UNIX - System V");
-			break;
-		case ELFOSABI_HPUX:
-			printf("UNIX - HP-UX");
-			break;
-		case ELFOSABI_NETBSD:
-			printf("UNIX - NetBSD");
-			break;
-		case ELFOSABI_LINUX:
-			printf("UNIX - GNU");
-			break;
-		case ELFOSABI_SOLARIS:
-			printf("UNIX - Solaris");
-			break;
-		case ELFOSABI_IRIX:
-			printf("UNIX - IRIX");
-			break;
-		case ELFOSABI_FREEBSD:
-			printf("UNIX - FreeBSD");
-			break;
-		case ELFOSABI_TRU64:
-			printf("UNIX - TRU64");
-			break;
-		default:
-			printf("<unknown: %x>", (unsigned int)buffer[7]);
-	}
-
-	printf("\n");
-}
-
-void printversion(char *buffer)
-{
-	ptitle("Version");
-
-	switch (buffer[6])
-	{
-		case EV_NONE:
-			printf("0");
-			break;
-		case EV_CURRENT:
-			printf("1 (current)");
-			break;
-		default:
-			printf("%d <unknown: %%lx>", (int)buffer[6]);
-	}
-
-	printf("\n");
-}
-
-void printdata(char *buffer)
-{
-	ptitle("Data");
-
-	switch (buffer[5])
-	{
-		case ELFDATANONE:
-			printf("none");
-			break;
-		case ELFDATA2LSB:
-			printf("2's complement, little endian");
-			break;
-		case ELFDATA2MSB:
-			printf("2's complement, big endian");
-			break;
-		default:
-			printf("<unknown: %x>", (unsigned int)buffer[5]);
-	}
-
-	printf("\n");
-}
-
-void printmagic(char *buffer)
-{
-	unsigned int i;
-
-	printf("  Magic:   ");
-	for (i = 0; i < 16; i++)
-		printf("%.2x ", buffer[i]);
-	printf("\n");
-}
-
-void printclass(char *buffer)
-{
-	ptitle("Class");
-
-	switch (buffer[4])
-	{
-		case ELFCLASSNONE:
-			printf("none");
-			break;
-		case ELFCLASS32:
-			printf("ELF32");
-			break;
-		case ELFCLASS64:
-			printf("ELF64");
-			break;
-		default:
-			printf("<unknown: %x>", (unsigned int)buffer[4]);
-	}
-	printf("\n");
-}
-
-void ptitle(char *title)
-{
-	unsigned int size = 37;
-	unsigned int tlen, i;
-
-	printf("  ");
-	printf("%s:", title);
-
-	for (tlen = 0; title[tlen]; tlen++)
-		;
-
-	for (i = 0; i < size - 3 - tlen; i++)
-		printf(" ");
 }
