e93fb4ad53005ba34d46f6dddc09788f5e0613de
smile9565 <bad9565@gmail.com>
2022-10-31 06:19:56 -0700

    corrected task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,46 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_binary - converts unsigned int to binary
- * @n: unsigned int
- * Return: binary
+ * _stoi - converts chars to ints
+ * @c: char to convert
+ * Return: converted int
  */
-void print_binary(unsigned long int n)
+unsigned int _stoi(char c)
 {
+	return ((unsigned int) c - '0');
+}
+/**
+ * _strlen - calculates the length of the string
+ * @s: input
+ * Return: length of string
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int i;
 
-	unsigned long int n_copy = n, mask = 1;
-	int len = 0;
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+/**
+ * binary_to_uint - converts a string of 1's and 0's to a decimal number
+ * @b: string to convert
+ * Return: unsigned decimal number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int result, tmp, expo;
 
-	while (n_copy > 0)
+	if (!b)
+		return (0);
+	result = tmp = 0;
+	expo = 1;
+	for (i = _strlen(b) - 1; b[i]; i--, expo *= 2)
 	{
-		len++;
-		n_copy >>= 1;
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		tmp = _stoi(b[i]);
+		result += tmp * expo;
 	}
-	len -= 1;
-
-	if (len > 0) /* create mask based on length of num */
-		mask = mask << len;
-
-	while (mask > 0) /* match each rightmost bit to see if 1 or 0 */
-	{
-		if (n & mask)
-			_putchar('1');
-		else
-			_putchar('0');
-
-		mask >>= 1;
-	}
+	return (result);
 }
